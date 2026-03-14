360ac40c7901c1bbc49645a5e1cb6afbb460fe02
Mayuri Phansalkar <mdp@bitadata.com>
2026-03-14 10:37:24 +0000

    test k8 changes

--- Websocket-Infra-Project-March-2026/terraform/modules/security-group/main.tf
+++ Websocket-Infra-Project-March-2026/terraform/modules/security-group/main.tf
@@ -23,6 +23,13 @@ resource "aws_security_group" "this" {
     self      = true
   }
 
+  ingress {
+    from_port   = 8472
+    to_port     = 8472
+    protocol    = "udp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
